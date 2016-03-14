155ae1568b07c53946b2d05357db23db02a65472
paola11 <lplf_girl@hotmail.com>
2016-03-13 22:28:42 -0600

    Agregar a formularioRecuperarContrasenaCorreo.php el codigo php del archivo doctype, modificar include por require_once y dejar solo el body

--- formularioRecuperarContrasenaCorreo.php
+++ formularioRecuperarContrasenaCorreo.php
@@ -1,43 +1,44 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <?php
-        include ('header.php');
-        ?>
-        <meta charset="UTF-8">
-        <title>Recuperar contraseña</title>
-        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    </head>
-    <body>
-        <div class="container main-pubIndex">
-            <div class="panel col-md-6 col-md-offset-3">
-                <div class="panel-heading postHeader">
-                    <div class="row">
-                        <h1 class="col-md-12">Recuperar contraseña</h1>
-                    </div>
-                </div>
-                <div class="panel-body postDesc">
-                   <p>Para restablecer tu contraseña, escribe el correo con el que te registraste.</p>
-                    <form class="form-horizontal">
+<?php
+require_once ('doctype.php');
+?>
 
-                        <div class="form-group">
-                            <label for="correo" class="col-xs-12 col-md-8 col-md-offset-2">Correo: </label>
-                            <div class="col-xs-12 col-md-8 col-md-offset-2">
-                                <input type="email" class="form-control" id="correo" placeholder="micuenta@ejemplo.com" />
-                            </div>
-                        </div>
+<body>
 
-                        <div class="form-group">
-                            <div class="col-xs-12 col-md-8 col-md-offset-2">
-                                <button type="button" onclick="location.href='formularioRecuperarContrasena.php';" class="btn btn-warning btn-block">Enviar</button>
-                            </div>
-                        </div>
-                    </form>
+    <?php
+    require_once ('header.php');
+    ?>
+
+    <div class="container main-pubIndex">
+        <div class="panel-default col-md-6 col-md-offset-3">
+            <div class="panel-heading postHeader">
+                <div class="row">
+                    <h1 class="col-md-12">Recuperar contraseña</h1>
                 </div>
             </div>
-        </div>  
-    </body>
-    <?php
-    include ('footer.php');
-    ?>
+            <div class="panel-body postDesc">
+                <p>Para restablecer tu contraseña, escribe el correo con el que te registraste.</p>
+                <form class="form-horizontal">
+
+                    <div class="form-group">
+                        <label for="correo" class="col-xs-12 col-md-8 col-md-offset-2">Correo: </label>
+                        <div class="col-xs-12 col-md-8 col-md-offset-2">
+                            <input type="email" class="form-control" id="correo" placeholder="micuenta@ejemplo.com" />
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <div class="col-xs-12 col-md-8 col-md-offset-2">
+                            <button type="button" onclick="location.href='formularioRecuperarContrasena.php';" class="btn btn-warning btn-block">Enviar</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>  
+</body>
+
+<?php
+require_once ('footer.php');
+?>
+
 </html>
