dfd84613a1d6d28d1a7de9deb7d1cb4d42b9f038
paola11 <lplf_girl@hotmail.com>
2016-03-13 19:30:17 -0600

    Agregar el codigo php del archivo doctype, modificar include por require_once y eliminar el código que se encuentra en estos archivos

--- index.php
+++ index.php
@@ -1,103 +1,97 @@
-<html lang="es">
-    <head>
-        <?php
-            include 'header.php';
-        ?>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-        <title>¡Bienvenido a Dragon art!</title>
+<?php
+require_once 'doctype.php';
+?>
 
-        <link type="text/css" href="css/jssor.css" rel="stylesheet" />
+<body class="index">
+   
+    <?php
+    require_once 'header.php';
+    ?>
+    
+    <div class="container warp">
 
-        <script type="text/javascript" src="js/jquery.min.js"></script>
-        <script type="text/javascript" src="js/jssor.slider.min.js"></script>
-        <script type="text/javascript" src="js/jssor.js"></script>
+        <div class="main main-usrIndex">
 
-    </head>
-    <body class="index">
-        
-        <div class="container warp">
-            
-            <div class="main main-usrIndex">
-                
-                <div class="col-md-12 col-slider">
-                    <div class="text-center">
-                        <h1>Bienvenido a DragonArt</h1>
-                        <h2>Comunidad donde artistas comparten sus creaciones.<br>¡Únete y comienza a explorar y compartir!</h2>
-                    </div>
-                    <div id="jssor_1" style="position: relative; margin: 0 auto; top: 0px; left: 0px; width: 800px; height: 456px; overflow: hidden; visibility: hidden; background-color: #24262e;">
-                            <!-- Loading Screen -->
-                            <div data-u="loading" style="position: absolute; top: 0px; left: 0px;">
-                                <div style="filter: alpha(opacity=70); opacity: 0.7; position: absolute; display: block; top: 0px; left: 0px; width: 100%; height: 100%;"></div>
-                                <div style="position:absolute;display:block;background:url('img/loading.gif') no-repeat center center;top:0px;left:0px;width:100%;height:100%;"></div>
-                            </div>
-                            <div data-u="slides" style="cursor: default; position: relative; top: 0px; left: 0px; width: 800px; height: 356px; overflow: hidden;">
-                                <div data-p="144.50" style="display: none;">
-                                    <a href="publicacionIndex.php"><img data-u="image" src="img/Imagen.png" /></a>
-                                    <img data-u="thumb" src="img/Imagen.png" />
-                                </div>
-                                <div data-p="144.50" style="display: none;">
-                                    <img data-u="image" src="img/Imagen2.png" />
-                                    <img data-u="thumb" src="img/Imagen2.png" />
-                                </div>
-                                <div data-p="144.50" style="display: none;">
-                                    <img data-u="image" src="img/Imagen3.png" />
-                                    <img data-u="thumb" src="img/Imagen3.png" />
-                                </div>
-                                <div data-p="144.50" style="display: none;">
-                                    <img data-u="image" src="img/Imagen4.jpg" />
-                                    <img data-u="thumb" src="img/Imagen4.jpg" />
-                                </div>
-                                <div data-p="144.50" style="display: none;">
-                                    <img data-u="image" src="img/Imagen5.png" />
-                                    <img data-u="thumb" src="img/Imagen5.png" />
-                                </div>
-                                <div data-p="144.50" style="display: none;">
-                                    <img data-u="image" src="img/Imagen6.png" />
-                                    <img data-u="thumb" src="img/Imagen6.png" />
-                                </div>
-                                <div data-p="144.50" style="display: none;">
-                                    <img data-u="image" src="img/Imagen7.png" />
-                                    <img data-u="thumb" src="img/Imagen7.png" />
-                                </div>
-                                <div data-p="144.50" style="display: none;">
-                                    <img data-u="image" src="img/Imagen8.png" />
-                                    <img data-u="thumb" src="img/Imagen8.png" />
-                                </div>
-                                <div data-p="144.50" style="display: none;">
-                                    <img data-u="image" src="img/Imagen9.png" />
-                                    <img data-u="thumb" src="img/Imagen9.png" />
-                                </div>
-                                <div data-p="144.50" style="display: none;">
-                                    <img data-u="image" src="img/Imagen10.png" />
-                                    <img data-u="thumb" src="img/Imagen10.png" />
-                                </div>
-                            </div>
-                            <!-- Thumbnail Navigator -->
-                            <div data-u="thumbnavigator" class="jssort01" style="position:absolute;left:0px;bottom:0px;width:800px;height:100px;" data-autocenter="1">
-                                <!-- Thumbnail Item Skin Begin -->
-                                <div data-u="slides" style="cursor: default;">
-                                    <div data-u="prototype" class="p">
-                                        <div class="w">
-                                            <div data-u="thumbnailtemplate" class="t"></div>
-                                        </div>
-                                        <div class="c"></div>
-                                    </div>
-                                </div>
-                                <!-- Thumbnail Item Skin End -->
-                            </div>
-                            <!-- Arrow Navigator -->
-                            <span data-u="arrowleft" class="jssora05l" style="top:158px;left:8px;width:40px;height:40px;"></span>
-                            <span data-u="arrowright" class="jssora05r" style="top:158px;right:8px;width:40px;height:40px;"></span>
-                        </div>
+            <div class="col-md-12 col-slider">
+                <div class="text-center">
+                    <h1>Bienvenido a DragonArt</h1>
+                    <h2>Comunidad donde artistas comparten sus creaciones.<br>¡Únete y comienza a explorar y compartir!</h2>
                 </div>
-
+                <div id="jssor_1" style="position: relative; margin: 0 auto; top: 0px; left: 0px; width: 800px; height: 456px; overflow: hidden; visibility: hidden; background-color: #24262e;">
+                    <!-- Loading Screen -->
+                    <div data-u="loading" style="position: absolute; top: 0px; left: 0px;">
+                        <div style="filter: alpha(opacity=70); opacity: 0.7; position: absolute; display: block; top: 0px; left: 0px; width: 100%; height: 100%;"></div>
+                        <div style="position:absolute;display:block;background:url('img/loading.gif') no-repeat center center;top:0px;left:0px;width:100%;height:100%;"></div>
+                    </div>
+                    <div data-u="slides" style="cursor: default; position: relative; top: 0px; left: 0px; width: 800px; height: 356px; overflow: hidden;">
+                        <div data-p="144.50" style="display: none;">
+                            <a href="publicacionIndex.php"><img data-u="image" src="img/Imagen.png" /></a>
+                            <img data-u="thumb" src="img/Imagen.png" />
+                        </div>
+                        <div data-p="144.50" style="display: none;">
+                            <img data-u="image" src="img/Imagen2.png" />
+                            <img data-u="thumb" src="img/Imagen2.png" />
+                        </div>
+                        <div data-p="144.50" style="display: none;">
+                            <img data-u="image" src="img/Imagen3.png" />
+                            <img data-u="thumb" src="img/Imagen3.png" />
+                        </div>
+                        <div data-p="144.50" style="display: none;">
+                            <img data-u="image" src="img/Imagen4.jpg" />
+                            <img data-u="thumb" src="img/Imagen4.jpg" />
+                        </div>
+                        <div data-p="144.50" style="display: none;">
+                            <img data-u="image" src="img/Imagen5.png" />
+                            <img data-u="thumb" src="img/Imagen5.png" />
+                        </div>
+                        <div data-p="144.50" style="display: none;">
+                            <img data-u="image" src="img/Imagen6.png" />
+                            <img data-u="thumb" src="img/Imagen6.png" />
+                        </div>
+                        <div data-p="144.50" style="display: none;">
+                            <img data-u="image" src="img/Imagen7.png" />
+                            <img data-u="thumb" src="img/Imagen7.png" />
+                        </div>
+                        <div data-p="144.50" style="display: none;">
+                            <img data-u="image" src="img/Imagen8.png" />
+                            <img data-u="thumb" src="img/Imagen8.png" />
+                        </div>
+                        <div data-p="144.50" style="display: none;">
+                            <img data-u="image" src="img/Imagen9.png" />
+                            <img data-u="thumb" src="img/Imagen9.png" />
+                        </div>
+                        <div data-p="144.50" style="display: none;">
+                            <img data-u="image" src="img/Imagen10.png" />
+                            <img data-u="thumb" src="img/Imagen10.png" />
+                        </div>
+                    </div>
+                    <!-- Thumbnail Navigator -->
+                    <div data-u="thumbnavigator" class="jssort01" style="position:absolute;left:0px;bottom:0px;width:800px;height:100px;" data-autocenter="1">
+                        <!-- Thumbnail Item Skin Begin -->
+                        <div data-u="slides" style="cursor: default;">
+                            <div data-u="prototype" class="p">
+                                <div class="w">
+                                    <div data-u="thumbnailtemplate" class="t"></div>
+                                </div>
+                                <div class="c"></div>
+                            </div>
+                        </div>
+                        <!-- Thumbnail Item Skin End -->
+                    </div>
+                    <!-- Arrow Navigator -->
+                    <span data-u="arrowleft" class="jssora05l" style="top:158px;left:8px;width:40px;height:40px;"></span>
+                    <span data-u="arrowright" class="jssora05r" style="top:158px;right:8px;width:40px;height:40px;"></span>
+                </div>
             </div>
 
         </div>
 
-    </body>
-    <?php
-        include 'footer.php';
-    ?>
+    </div>
+
+</body>
+
+<?php
+require_once 'footer.php';
+?>
+
 </html>
