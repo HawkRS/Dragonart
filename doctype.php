a9a030d10ac98dcbdbbdd30bf3ec29fca0b61fba
paola11 <lplf_girl@hotmail.com>
2016-04-02 17:41:09 -0600

    Agregar a terminosCondiciones.php el codigo php del archivo doctype, modificar include por require_onde y dejar solo el body. Agregar al archivo doctype.php el link para la hoja de estilo que usa el archivo terminosCondiciones.php

--- doctype.php
+++ doctype.php
@@ -3,15 +3,18 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-        <link rel="stylesheet" href="css/bootstrap.min.css">
-        <link rel="stylesheet" href="css/estilo.css">
-        <link type="text/css" href="css/jssor.css" rel="stylesheet" />
+        <link type="text/css" rel="stylesheet" href="css/bootstrap.min.css">
+        <link type="text/css" rel="stylesheet" href="css/estilo.css">
+        <link type="text/css" rel="stylesheet" href="css/simple-sidebar.css">
+        <link type="text/css" rel="stylesheet" href="css/jssor.css" />
+        <link type="text/css" rel="stylesheet" href="css/star-rating.css" />
+        <link type="text/css" rel="stylesheet" href="css/theme-krajee-svg.css" />
+        
         <link href='https://fonts.googleapis.com/css?family=Noto+Sans' rel='stylesheet' type='text/css'>
         <style>
             body{
                 font-family: 'Noto Sans', sans-serif;
             }
         </style>
-        <link href="css/star-rating.css" media="all" rel="stylesheet" type="text/css" />
-        <link href="css/theme-krajee-svg.css" media="all" rel="stylesheet" type="text/css" />
+        
     </head>
