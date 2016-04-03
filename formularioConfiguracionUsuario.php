4affc3f6487bac0990703dea6eabccf28ae5e87c
paola11 <lplf_girl@hotmail.com>
2016-04-02 20:07:25 -0600

    Agregar type a los botones y name a los input, cambiar las columnas de bootstrap, poner method y action a la etiqueta form para no repetirlos en cada elemento de formularioConfiguracionUsuario.php

--- formularioConfiguracionUsuario.php
+++ formularioConfiguracionUsuario.php
@@ -14,57 +14,56 @@ require_once ('doctype.php');
                     <h1 class="col-md-12">Editar perfil</h1>
                 </div>
             </div>
+            
             <div class="panel-body postDesc">
-                <form class="form-horizontal" enctype="multipart/form-data">
+                <form class="form-horizontal col-xs-12 col-md-8 col-md-offset-2" enctype="multipart/form-data" action="" method="post">
 
                     <div class="form-group">
-                        <label for="nombre" class="col-xs-12 col-md-8 col-md-offset-2">Nombre: </label>
-                        <div class="col-xs-12 col-md-8 col-md-offset-2">
+                        <label for="nombre">Nombre: </label>
+                        <div>
                             <input type="text" class="form-control" id="nombre" placeholder="Nombre" name="nombre" />
                         </div>
                     </div>
 
                     <div class="form-group">
-                        <label for="alias" class="col-xs-12 col-md-8 col-md-offset-2">Alias: </label>
-                        <div class="col-xs-12 col-md-8 col-md-offset-2">
+                        <label for="alias">Alias: </label>
+                        <div>
                             <input type="text" class="form-control" id="alias" placeholder="Alias" name="alias" />
                         </div>
                     </div>
 
                     <div class="form-group">
-                        <label for="correo" class="col-xs-12 col-md-8 col-md-offset-2">Correo: </label>
-                        <div class="col-xs-12 col-md-8 col-md-offset-2">
+                        <label for="correo">Correo: </label>
+                        <div>
                             <input type="email" class="form-control" id="correo" placeholder="alguien@ejemplo.com" disabled name="correo" />
                         </div>
                     </div>
 
                     <div class="form-group">
-                        <label for="password" class="col-xs-12 col-md-8 col-md-offset-2">Contraseña: </label>
-                        <div class="col-xs-12 col-md-8 col-md-offset-2">
+                        <label for="password">Contraseña: </label>
+                        <div>
                             <input type="password" class="form-control" id="password" placeholder="Contreseña" name="password" />
                         </div>
                     </div>
 
                     <div class="form-group">
-                        <label for="passwordConfirmacion" class="col-xs-12 col-md-8 col-md-offset-2">Repetir contraseña: </label>
-                        <div class="col-xs-12 col-md-8 col-md-offset-2">
+                        <label for="passwordConfirmacion">Repetir contraseña: </label>
+                        <div>
                             <input type="password" class="form-control" id="passwordConfirmacion" placeholder="Repetir contreseña" name="passwordConfirmacion" />
                         </div>
                     </div>
 
                     <div class="form-group">
-                        <label for="biografia" class="col-xs-12 col-md-8 col-md-offset-2">Biografía: </label>
-                        <div class="col-xs-12 col-md-8 col-md-offset-2">
-                            <textarea class="form-control" id="biografia" rows="5" name="biografia"></textarea>
+                        <label for="biografia">Biografía: </label>
+                        <div>
+                            <textarea class="form-control col-xs-12 col-md-8" id="biografia" name="biografia"></textarea>
                         </div>
                     </div>
 
                     <div class="form-group">
-                        <label for="avatar" class="col-xs-12 col-md-8 col-md-offset-2">Avatar: </label>
-                        <div class="col-xs-12 col-md-4 col-md-offset-3">
-                            <input type="file" name="filename" class="form-control" id="avatar" accept="image/gif, image/jpeg, image/png, image/jpg" onchange="readURL(this);">
-                        </div>
-                        <div class="col-xs-12 col-md-4">
+                        <label for="avatar">Avatar: </label>
+                        <div>
+                           <input type="file" name="filename" class="form-control" id="avatar" accept="image/gif, image/jpeg, image/png, image/jpg" onchange="readURL(this);">
                             <img id="blah" src="#" alt="Avatar" width="100" height="100" />
                         </div>
                     </div>
