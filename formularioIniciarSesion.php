4cc1df7e89ab0db4ec4d56c21de7ec44a4945a0b
paola11 <lplf_girl@hotmail.com>
2016-04-02 20:56:04 -0600

    Agregar action y method, organizar las columnas de bootstrap para que no se repitan en cada elemento del formulario, agregar name a los input y type al boton en formularioIniciarSesion.php

--- formularioIniciarSesion.php
+++ formularioIniciarSesion.php
@@ -16,31 +16,32 @@ require_once ('doctype.php');
                 </div>
             </div>
             <div class="panel-body postDesc">
-                <form class="form-horizontal">
+                <form class="form-horizontal col-xs-12 col-md-12">
 
                     <div class="form-group">
-                        <label for="correo" class="col-xs-12 col-md-8 col-md-offset-2">Correo: </label>
-                        <div class="col-xs-12 col-md-8 col-md-offset-2">
-                            <input type="email" class="form-control" id="correo" placeholder="alguien@ejemplo.com" />
+                        <label for="correo">Correo: </label>
+                        <div>
+                            <input type="email" class="form-control" id="correo" placeholder="alguien@ejemplo.com" name="correo" />
                         </div>
                     </div>
 
                     <div class="form-group">
-                        <label for="password" class="col-xs-12 col-md-8 col-md-offset-2">Contraseña: </label>
-                        <div class="col-xs-12 col-md-8 col-md-offset-2">
-                            <input type="password" class="form-control" id="password" placeholder="Contreseña" />
+                        <label for="password">Contraseña: </label>
+                        <div>
+                            <input type="password" class="form-control" id="password" placeholder="Contreseña" name="password" />
                         </div>
                     </div>
 
                     <div class="form-group">
                         <div class="col-xs-12 col-md-8 col-md-offset-2">
-                            <button type="button" onclick="location.href='usuarioIndex.php';" class="btn btn-warning btn-block">Iniciar sesión</button>
+                            <button type="submit" onclick="location.href='usuarioIndex.php';" class="btn btn-warning btn-block">Iniciar sesión</button>
                         </div>
                     </div>
                 </form>
                 <span><small><br>¿Aún no estás registrado en Dragonart? <a href="formularioRegistrarUsuario.php">Regístrate ahora</a></small></span><br>
                 <span><small>¿Olvidaste tu contraseña? Recupérala <a href="formularioRecuperarContrasenaCorreo.php">aquí</a></small></span>
             </div>
+            
         </div>
     </div>  
 </body>
