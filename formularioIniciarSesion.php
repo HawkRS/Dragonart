658299035abd91e5fa53cb3e7e408d14de5cb84f
Mauricio Gerardo Lazcano Origel <dragon.mauri94@gmail.com>
2016-04-04 18:52:48 -0500

    Se movieron los botones de inicio de sesión redes sociales hacia abajo

--- formularioIniciarSesion.php
+++ formularioIniciarSesion.php
@@ -9,53 +9,55 @@ require_once ('doctype.php');
     ?>
 
     <div class="container main-pubIndex">
-        <div class="col-md-6 col-md-offset-3 panel-default">
-            <div class="panel-heading postHeader">
-                <div class="row">
-                    <h1 class="col-md-12">Iniciar Sesión</h1>
+        <div class="col-md-6 col-md-offset-3">
+            <div class="panel panel-default">
+                <div class="panel-heading postHeader">
+                    <div class="row">
+                        <h1 class="col-md-12">Iniciar Sesión</h1>
+                    </div>
                 </div>
-            </div>
 
-            <div class="sociales col-xs-12 col-md-8 col-md-offset-2">
-                <a class="btn btn-block btn-social btn-facebook">
-                    <span class="fa fa-facebook"></span> Iniciar sesión con Facebook
-                </a>
-
-                <a class="btn btn-block btn-social btn-twitter">
-                    <span class="fa fa-twitter"></span> Iniciar sesión con Twitter
-                </a>
-
-                <a class="btn btn-block btn-social btn-google">
-                    <span class="fa fa-google"></span> Iniciar sesión con Google
-                </a>
-            </div>
-
-            <div class="panel-body postDesc">
-                <form class="form-horizontal col-xs-12 col-md-12">
-                    <div class="form-group">
-                        <label for="correo">Correo: </label>
-                        <div>
-                            <input type="email" class="form-control" id="correo" placeholder="alguien@ejemplo.com" name="correo" />
+                <div class="panel-body postDesc">
+                    <form class="form-horizontal col-xs-12 col-md-12">
+                        <div class="form-group">
+                            <label for="correo">Correo: </label>
+                            <div>
+                                <input type="email" class="form-control" id="correo" placeholder="alguien@ejemplo.com" name="correo" />
+                            </div>
                         </div>
-                    </div>
 
-                    <div class="form-group">
-                        <label for="password">Contraseña: </label>
-                        <div>
-                            <input type="password" class="form-control" id="password" placeholder="Contreseña" name="password" />
+                        <div class="form-group">
+                            <label for="password">Contraseña: </label>
+                            <div>
+                                <input type="password" class="form-control" id="password" placeholder="Contreseña" name="password" />
+                            </div>
                         </div>
-                    </div>
 
-                    <div class="form-group">
-                        <div class="col-xs-12 col-md-8 col-md-offset-2">
-                            <button type="submit" onclick="location.href='usuarioIndex.php';" class="btn btn-warning btn-block">Iniciar sesión</button>
+                        <div class="form-group">
+                            <div class="col-xs-12 col-md-8 col-md-offset-2">
+                                <button type="submit" onclick="location.href='usuarioIndex.php';" class="btn btn-warning btn-block">Iniciar sesión</button>
+                            </div>
                         </div>
-                    </div>
-                    <div class="form-group">
-                        <span><small>¿Aún no estás registrado en Dragonart? <a href="formularioRegistrarUsuario.php">Regístrate ahora</a></small></span><br>
-                        <span><small>¿Olvidaste tu contraseña? Recupérala <a href="formularioRecuperarContrasenaCorreo.php">aquí</a></small></span>
-                    </div>
-                </form>                
+                        <div class="form-group">
+                            <span><small>¿Aún no estás registrado en Dragonart? <a href="formularioRegistrarUsuario.php">Regístrate ahora</a></small></span><br>
+                            <span><small>¿Olvidaste tu contraseña? Recupérala <a href="formularioRecuperarContrasenaCorreo.php">aquí</a></small></span>
+                        </div>
+                    </form>                
+                </div>
+
+                <div class="sociales col-xs-12 col-md-8 col-md-offset-2">
+                    <a class="btn btn-block btn-social btn-facebook">
+                        <span class="fa fa-facebook"></span> Iniciar sesión con Facebook
+                    </a>
+
+                    <a class="btn btn-block btn-social btn-twitter">
+                        <span class="fa fa-twitter"></span> Iniciar sesión con Twitter
+                    </a>
+
+                    <a class="btn btn-block btn-social btn-google">
+                        <span class="fa fa-google"></span> Iniciar sesión con Google+
+                    </a>
+                </div>
             </div>
         </div>
     </div>  
