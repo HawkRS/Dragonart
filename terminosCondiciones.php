ae1fd624e021686028f80537f512b9abdab5ff0e
Mauricio Gerardo Lazcano Origel <dragon.mauri94@gmail.com>
2016-04-04 20:47:32 -0500

    Agregado el icono para el sidebar de terminos y condiciones

--- terminosCondiciones.php
+++ terminosCondiciones.php
@@ -10,11 +10,10 @@ require_once 'doctype.php';
 
     <div id="wrapper" class="main-otro">
         <aside id="sidebar-wrapper">
+
             <nav>
                 <ul class="sidebar-nav">
-                    <li class="sidebar-brand">
-                        <h1 class="titulo">Opciones</h1>
-                    </li>
+                    <li class="sidebar-brand"><span>Opciones</span></li>
                     <li><a href="#punto1">Opcion 1</a></li>
                     <li><a href="#punto2">Opcion 2</a></li>
                     <li><a href="#punto3">Opcion 3</a></li>
@@ -29,7 +28,7 @@ require_once 'doctype.php';
                 <div class="row">
                     <div class="col-md-12">
                         <h1>Términos y condiciones</h1>
-                        <a href="#menu-toggle" class="btn btn-warning" id="menu-toggle">Desplegar menú</a>
+                        <a href="#menu-toggle" class="btn btn-warning" id="menu-toggle"><i class="glyphicon glyphicon-chevron-left"></i></a>
                         <a href="#punto1"><h2>Punto 1</h2></a>
                         <p>
                             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos ullam quidem velit fugiat distinctio inventore perspiciatis eaque sed repellat dolorem. Dolore rerum vitae laborum veniam corporis repellendus aliquam accusantium voluptates.
