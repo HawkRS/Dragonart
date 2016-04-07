bac7f1bf4dbb34700f116f6d536c69c2460c0c44
Mauricio Gerardo Lazcano Origel <dragon.mauri94@gmail.com>
2016-04-06 21:05:14 -0500

    Cambio de diseño de la página de usuario

--- usuarioIndex.php
+++ usuarioIndex.php
@@ -8,36 +8,155 @@ require_once 'doctype.php';
     require_once 'header.php';
     ?>
 
-    <div class="container warp">
-        <div class="main main-usrIndex">
-            <div class="col-md-4">
-
-                <div class="panel panel-default">
-                    <div class="panel-heading usrHeader">
+    <div id="wrapper" class="main-otro">
+        <aside id="sidebar-wrapper">
+            <nav>
+                <ul class="sidebar-nav">
+                    <figure>
                         <picture>
                             <img class="center-block avatar" src="img/avatar.png" alt="Nombre de usuario">
                         </picture>
-
-                        <section class="text-center">
-                            <h1>Nombre de usuario</h1>
-                            <button class="btn btn-warning btn-lg btn-block" name="seguir" id="seguir"><span class="glyphicon glyphicon-plus-sign"></span> Seguir</button>
-                            <button class="btn btn-danger btn-lg btn-block" name="seguir" id="seguir"><span class="glyphicon glyphicon-remove-sign"></span> Bloquear usuario</button>
-                        </section>
-                    </div>
-
-                    <div class="panel-body postDesc">
-                        <p>
+                    </figure>
+                    <h2 class="text-center blanco">Nombre de usuario</h2>
+                    <div class="col-xs-12">
+                        <button class="btn btn-warning btn-lg btn-block" name="seguir" id="seguir"><span class="glyphicon glyphicon-plus-sign"></span> Seguir</button>
+                        <button class="btn btn-danger btn-lg btn-block" name="seguir" id="seguir"><span class="glyphicon glyphicon-remove-sign"></span> Bloquear usuario</button>
+                        <p class="extra-space text-justify blanco">
                             Descripción del usuario: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis beatae excepturi minus incidunt, sequi odio ut veniam officia ea architecto qui sint nostrum inventore labore iure natus, vel dolor ullam.
                         </p>
                     </div>
+                </ul>
+            </nav>
+        </aside>
 
-                    <div class="panel-footer postTags">
-                        <span>Registrado desde: 28/feb/2016</span>
-                        <br>
-                        <span>Imágenes publicadas: 123</span>
+        <section id="page-content-wrapper">
+            <div class="container-fluid">
+                <div class="extra-space">
+                    <a href="#menu-toggle" class="btn btn-warning" id="menu-toggle"><i class="glyphicon glyphicon-chevron-left"></i></a>
+                </div>
+                <div class="extra-space">
+                    <div class="panel panel-default">
+
+                        <div class="panel-heading galHeader">
+                            <h2 class="galHeaderText">Galería</h2>
+                        </div>
+
+                        <div class="panel-body postDesc">
+
+                            <div class="row">
+
+                                <div class="col-sm-6 col-md-3">
+                                    <div class="thumbnail">
+                                        <a href="publicacionIndex.php">
+                                            <img src="img/Imagen.png" alt="Demostración">
+                                        </a>
+                                        <div class="caption">
+                                            <h3>Título de la imagen</h3>
+                                            <button class="btn btn-warning btn-block" name="favorito" id="favorito">+Favorito</button>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="col-sm-6 col-md-3">
+                                    <div class="thumbnail">
+                                        <img src="img/Imagen4.jpg" alt="Demostración">
+                                        <div class="caption">
+                                            <h3>Título de la imagen</h3>
+                                            <button class="btn btn-warning btn-block" name="favorito" id="favorito">+Favorito</button>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="col-sm-6 col-md-3">
+                                    <div class="thumbnail">
+                                        <img src="img/Imagen8.png" alt="Demostración">
+                                        <div class="caption">
+                                            <h3>Título de la imagen</h3>
+                                            <button class="btn btn-warning btn-block" name="favorito" id="favorito">+Favorito</button>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="col-sm-6 col-md-3">
+                                    <div class="thumbnail">
+                                        <img src="img/Imagen7.png" alt="Demostración">
+                                        <div class="caption">
+                                            <h3>Título de la imagen</h3>
+                                            <button class="btn btn-warning btn-block" name="favorito" id="favorito">+Favorito</button>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+
+                        </div>
+
+                        <div class="panel-footer postTags">
+                            <button class="btn btn-warning" name="seguidores" id="seguidores"><span class="glyphicon glyphicon-plus-sign"></span> Ver mas</button>
+                        </div>
+
                     </div>
                 </div>
 
+                <div class="panel panel-default">
+
+                    <div class="panel-heading galHeader">
+                        <h2 class="galHeaderText">Favoritos</h2>
+                    </div>
+
+                    <div class="panel-body postDesc">
+
+                        <div class="row">
+
+                            <div class="col-sm-6 col-md-3">
+                                <div class="thumbnail">
+                                    <img src="img/Imagen2.png" alt="Demostración">
+                                    <div class="caption">
+                                        <h3>Título de la imagen</h3>
+                                        <button class="btn btn-warning btn-block" name="favorito" id="favorito">+Favorito</button>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-sm-6 col-md-3">
+                                <div class="thumbnail">
+                                    <img src="img/Imagen3.png" alt="Demostración">
+                                    <div class="caption">
+                                        <h3>Título de la imagen</h3>
+                                        <button class="btn btn-warning btn-block" name="favorito" id="favorito">+Favorito</button>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-sm-6 col-md-3">
+                                <div class="thumbnail">
+                                    <img src="img/Imagen10.png" alt="Demostración">
+                                    <div class="caption">
+                                        <h3>Título de la imagen</h3>
+                                        <button class="btn btn-warning btn-block" name="favorito" id="favorito">+Favorito</button>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-sm-6 col-md-3">
+                                <div class="thumbnail">
+                                    <img src="img/Imagen5.png" alt="Demostración">
+                                    <div class="caption">
+                                        <h3>Título de la imagen</h3>
+                                        <button class="btn btn-warning btn-block" name="favorito" id="favorito">+Favorito</button>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                    <div class="panel-footer postTags">
+                        <button class="btn btn-warning" name="seguidores" id="seguidores"><span class="glyphicon glyphicon-plus-sign"></span> Ver mas</button>
+                    </div>
+
+                </div>
                 <div class="panel panel-default">
                     <div class="panel-heading galHeader">
                         <h2 class="galHeaderText">Seguidores</h2>
@@ -137,140 +256,20 @@ require_once 'doctype.php';
                         <button class="btn btn-warning" name="seguidores" id="seguidores"><span class="glyphicon glyphicon-plus-sign"></span> Ver mas</button>
                     </div>
                 </div>
-
             </div>
-
-            <div class="col-md-8">
-
-                <div class="panel panel-default">
-
-                    <div class="panel-heading galHeader">
-                        <h2 class="galHeaderText">Galería</h2>
-                    </div>
-
-                    <div class="panel-body postDesc">
-
-                        <div class="row">
-
-                            <div class="col-sm-6 col-md-3">
-                                <div class="thumbnail">
-                                    <a href="publicacionIndex.php">
-                                        <img src="img/Imagen.png" alt="Demostración">
-                                    </a>
-                                    <div class="caption">
-                                        <h3>Título de la imagen</h3>
-                                        <button class="btn btn-warning btn-block" name="favorito" id="favorito">+Favorito</button>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="col-sm-6 col-md-3">
-                                <div class="thumbnail">
-                                    <img src="img/Imagen4.jpg" alt="Demostración">
-                                    <div class="caption">
-                                        <h3>Título de la imagen</h3>
-                                        <button class="btn btn-warning btn-block" name="favorito" id="favorito">+Favorito</button>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="col-sm-6 col-md-3">
-                                <div class="thumbnail">
-                                    <img src="img/Imagen8.png" alt="Demostración">
-                                    <div class="caption">
-                                        <h3>Título de la imagen</h3>
-                                        <button class="btn btn-warning btn-block" name="favorito" id="favorito">+Favorito</button>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="col-sm-6 col-md-3">
-                                <div class="thumbnail">
-                                    <img src="img/Imagen7.png" alt="Demostración">
-                                    <div class="caption">
-                                        <h3>Título de la imagen</h3>
-                                        <button class="btn btn-warning btn-block" name="favorito" id="favorito">+Favorito</button>
-                                    </div>
-                                </div>
-                            </div>
-
-                        </div>
-
-                    </div>
-
-                    <div class="panel-footer postTags">
-                        <button class="btn btn-warning" name="seguidores" id="seguidores"><span class="glyphicon glyphicon-plus-sign"></span> Ver mas</button>
-                    </div>
-
-                </div>
-
-                <div class="panel panel-default">
-
-                    <div class="panel-heading galHeader">
-                        <h2 class="galHeaderText">Favoritos</h2>
-                    </div>
-
-                    <div class="panel-body postDesc">
-
-                        <div class="row">
-
-                            <div class="col-sm-6 col-md-3">
-                                <div class="thumbnail">
-                                    <img src="img/Imagen2.png" alt="Demostración">
-                                    <div class="caption">
-                                        <h3>Título de la imagen</h3>
-                                        <button class="btn btn-warning btn-block" name="favorito" id="favorito">+Favorito</button>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="col-sm-6 col-md-3">
-                                <div class="thumbnail">
-                                    <img src="img/Imagen3.png" alt="Demostración">
-                                    <div class="caption">
-                                        <h3>Título de la imagen</h3>
-                                        <button class="btn btn-warning btn-block" name="favorito" id="favorito">+Favorito</button>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="col-sm-6 col-md-3">
-                                <div class="thumbnail">
-                                    <img src="img/Imagen10.png" alt="Demostración">
-                                    <div class="caption">
-                                        <h3>Título de la imagen</h3>
-                                        <button class="btn btn-warning btn-block" name="favorito" id="favorito">+Favorito</button>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="col-sm-6 col-md-3">
-                                <div class="thumbnail">
-                                    <img src="img/Imagen5.png" alt="Demostración">
-                                    <div class="caption">
-                                        <h3>Título de la imagen</h3>
-                                        <button class="btn btn-warning btn-block" name="favorito" id="favorito">+Favorito</button>
-                                    </div>
-                                </div>
-                            </div>
-
-                        </div>
-
-                    </div>
-
-                    <div class="panel-footer postTags">
-                        <button class="btn btn-warning" name="seguidores" id="seguidores"><span class="glyphicon glyphicon-plus-sign"></span> Ver mas</button>
-                    </div>
-
-                </div>
-
-            </div>
-        </div>
+        </section>
     </div>
 </body>
 
-<?php 
+<?php
 require_once 'footer.php';
 ?>
+
+<script>
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
+</script>
 
 </html>
