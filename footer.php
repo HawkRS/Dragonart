147a1914b4639c4f1287fa0b76bd36fd7586b190
paola11 <lplf_girl@hotmail.com>
2016-04-09 22:18:14 -0500

    Modificacion del footer, se agrego nuevo codigo y se comentó el anterior

--- footer.php
+++ footer.php
@@ -1,21 +1,40 @@
+<footer>
+    <div class="container-fluid footerInfo">
+        <div class="row">
+            <div class="col-md-12 text-center">
+                &copy; 2016. Digital Dragon. 
+                <span>
+                    <span>|</span>
+                    <a href="terminosCondiciones.php">Términos y condiciones</a>
+                    <span>|</span>
+                    <a href="politicaCargaImagenes.php">Política de carga de imágenes</a>
+                    <span>|</span>
+                    <a href="formularioContacto.php">Contacto</a>
+                </span>
+            </div>
+
+        </div>
+    </div>
+</footer>
+
 <script type="text/javascript" src="js/jssor.slider.min.js"></script>
 <script type="text/javascript" src="js/jssor.js"></script>
 <script type="text/javascript" src="js/bootstrap.min.js"></script>
 <script type="text/javascript" src="js/avatar.js"></script>
 <script type="text/javascript" src="js/imagen.js"></script>
 
-<footer class="footer">
-    <div class="container-fluid text-center footerInfo footerInfo-sm">
-        <nav>
-            <span>&copy;2016 Digital Dragon</span>
-            <span>
-                <span>|</span>
-                <a href="terminosCondiciones.php">Términos y condiciones</a>
-                <span>|</span>
-                <a href="politicaCargaImagenes.php">Política de carga de imágenes</a>
-                <span>|</span>
-                <a href="formularioContacto.php">Contacto</a>
-            </span>
-        </nav>
-    </div>
-</footer>
+<!--<footer class="footer">
+<div class="container-fluid text-center footerInfo footerInfo-sm">
+<nav>
+<span>&copy;2016 Digital Dragon</span>
+<span>
+<span>|</span>
+<a href="terminosCondiciones.php">Términos y condiciones</a>
+<span>|</span>
+<a href="politicaCargaImagenes.php">Política de carga de imágenes</a>
+<span>|</span>
+<a href="formularioContacto.php">Contacto</a>
+</span>
+</nav>
+</div>
+</footer>-->
