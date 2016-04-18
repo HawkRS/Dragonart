7874266450a8955cce0f2c5381846092cd09bb18
paola11 <lplf_girl@hotmail.com>
2016-04-18 10:26:01 -0500

    Modificar la visualizacion de las politicas de carga de imagenes

--- politicaCargaImagenes.php
+++ politicaCargaImagenes.php
@@ -7,34 +7,47 @@ require_once 'doctype.php';
     <?php
     require_once 'header.php';
     ?>
+
+    <div class="container main-pubIndex">
+        <div class="panel panel-default">
+            <div class="panel-heading postHeader">
+                <div class="row">
+                    <h1 class="col-md-12">Política de carga de imágenes</h1>
+                </div>
+            </div>
+            <div class="panel-body postDesc">
+                <h2>Punto 1</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos ullam quidem velit fugiat distinctio inventore perspiciatis eaque sed repellat dolorem. Dolore rerum vitae laborum veniam corporis repellendus aliquam accusantium voluptates.
+                </p>
+                <h2>Punto 2</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos ullam quidem velit fugiat distinctio inventore perspiciatis eaque sed repellat dolorem. Dolore rerum vitae laborum veniam corporis repellendus aliquam accusantium voluptates.
+                </p>
+                <h2>Punto 3</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos ullam quidem velit fugiat distinctio inventore perspiciatis eaque sed repellat dolorem. Dolore rerum vitae laborum veniam corporis repellendus aliquam accusantium voluptates.
+                </p>
+                <h2>Punto 4</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos ullam quidem velit fugiat distinctio inventore perspiciatis eaque sed repellat dolorem. Dolore rerum vitae laborum veniam corporis repellendus aliquam accusantium voluptates.
+                </p>
+                <h2>Punto 5</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos ullam quidem velit fugiat distinctio inventore 
     <div class="main-pubIndex">
         <section>
             <div class="container-fluid">
                 <div class="col-md-10">
-                    <h1>Política de carga de imágenes</h1>
-                    <h2>Punto 1</h2>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos ullam quidem velit fugiat distinctio inventore perspiciatis eaque sed repellat dolorem. Dolore rerum vitae laborum veniam corporis repellendus aliquam accusantium voluptates.
-                    </p>
-                    <h2>Punto 2</h2>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos ullam quidem velit fugiat distinctio inventore perspiciatis eaque sed repellat dolorem. Dolore rerum vitae laborum veniam corporis repellendus aliquam accusantium voluptates.
-                    </p>
-                    <h2>Punto 3</h2>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos ullam quidem velit fugiat distinctio inventore perspiciatis eaque sed repellat dolorem. Dolore rerum vitae laborum veniam corporis repellendus aliquam accusantium voluptates.
-                    </p>
-                    <h2>Punto 4</h2>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos ullam quidem velit fugiat distinctio inventore perspiciatis eaque sed repellat dolorem. Dolore rerum vitae laborum veniam corporis repellendus aliquam accusantium voluptates.
-                    </p>
-                    <h2>Punto 5</h2>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos ullam quidem velit fugiat distinctio inventore perspiciatis eaque sed repellat dolorem. Dolore rerum vitae laborum veniam corporis repellendus aliquam accusantium voluptates.
-                    </p>
+                    <h1></h1>
+                    
                 </div>
             </div>
         </section>
+    </div>perspiciatis eaque sed repellat dolorem. Dolore rerum vitae laborum veniam corporis repellendus aliquam accusantium voluptates.
+                </p>
+            </div>
+        </div>
     </div>
 </body>
 
