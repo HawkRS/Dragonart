90ea772645084450e9adba93403111ec6c984b24
paola11 <lplf_girl@hotmail.com>
2016-05-20 22:58:06 -0500

    Agregar las líneas necesarias para poder abrir de manera adecuada la vista de contacto con la accion mostrar

--- app/controladores/contactoCtl.php
+++ app/controladores/contactoCtl.php
@@ -2,10 +2,17 @@
 
 class contactoCtl {
     public $modelo;
+    
+    private $doctype;
+    private $header;
+    private $footer;
 
     function __construct() {
     	session_start();
         echo 'Soy contactoCtl';
+        $this->doctype = file_get_contents('app/vistas/doctype.html');
+        $this->header = file_get_contents('app/vistas/header.html');
+        $this->footer = file_get_contents('app/vistas/footer.html');
     }
 
     function run() {
@@ -21,7 +28,14 @@ class contactoCtl {
     }
 
     function mostrar() {
-        require_once('app/vistas/formularioContacto.php');
+         $vista = file_get_contents('app/vistas/formularioContacto.html');
+        $inicioFooter = strpos($vista, '<!--inicioFooter-->');
+        $finFooter = strpos($vista, '<!--finFooter-->')+16;
+        $remplazar = substr($vista,$inicioFooter,$finFooter-$inicioFooter);
+        
+        $vista = str_replace($remplazar, $this->footer, $vista);
+        $vista = $this->doctype.$this->header.$vista;
+        echo $vista;
     }
 }
 ?>
