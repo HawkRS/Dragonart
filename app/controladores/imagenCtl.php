dd3b76de00d90441c4b82d3d73a522b181cc8b51
paola11 <lplf_girl@hotmail.com>
2016-05-20 23:17:49 -0500

    Agregar las líneas necesarias para poder abrir de manera adecuada la vista de la principal con la accion mostrar

--- app/controladores/imagenCtl.php
+++ app/controladores/imagenCtl.php
@@ -2,10 +2,17 @@
 
 class imagenCtl {
     public $modelo;
+    
+    private $doctype;
+    private $header;
+    private $footer;
 
     function __construct() {
     	session_start();
         echo 'Soy imagenCtl';
+        $this->doctype = file_get_contents('app/vistas/doctype.html');
+        $this->header = file_get_contents('app/vistas/header.html');
+        $this->footer = file_get_contents('app/vistas/footer.html');
     }
 
     function run() {
@@ -34,7 +41,20 @@ class imagenCtl {
     }
 
     function inicio() {
-        require_once('app/vistas/principal.php');
+        $vista = file_get_contents('app/vistas/principal.html');
+        $inicioBody = strpos($this->doctype, '<!--inicioBody-->');
+        $finBody = strpos($this->doctype, '<!--finBody-->')+14;
+        $inicioFooter = strpos($vista, '<!--inicioFooter-->');
+        $finFooter = strpos($vista, '<!--finFooter-->')+16;
+        
+        $remplazarBody = substr($this->doctype,$inicioBody,$finBody-$inicioBody); 
+        $remplazarFooter = substr($vista,$inicioFooter,$finFooter-$inicioFooter);
+        
+        $this->doctype = str_replace($remplazarBody, '<body class="index">', $this->doctype);        
+        $vista = str_replace($remplazarFooter, $this->footer, $vista);
+        
+        $vista = $this->doctype.$this->header.$vista;
+        echo $vista;
     }
     
     function mostrar() {
