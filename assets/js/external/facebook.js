34254c6ad19f3e42815585ad498e0ea39aea25a1
paola11 <lplf_girl@hotmail.com>
2016-05-28 23:32:44 -0500

    Agregar archivo para manejo de inicio de sesion con facebook

---
+++ assets/js/external/facebook.js
@@ -0,0 +1,37 @@
+window.fbAsyncInit = function() {
+    FB.init({
+        appId      : '1744902582421509',
+        cookie     : true,
+        xfbml      : true,
+        version    : 'v2.6'
+    });
+};
+
+(function(d, s, id){
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {return;}
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+function ingresarFacebook() {  
+    FB.login(function(response){  
+        validarUsuario();  
+    }, {scope: 'public_profile, email'});  
+}
+
+function validarUsuario() {  
+    FB.getLoginStatus(function(response) {  
+        if(response.status == 'connected') {
+            FB.api('/me?fields=id,name,email', function(response) {
+                console.log(JSON.stringify(response));
+                alert('Hola ' + response.email);
+            });   
+        } else if(response.status == 'not_authorized') {  
+            alert('Debes de dar permisos a DragonArt para el manejo de tu información');  
+        } else {  
+            alert('Debes de iniciar sesión en Facebook');  
+        }  
+    });  
+}  
